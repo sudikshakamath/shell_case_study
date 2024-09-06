e6cb754ed72bf05e8e20bf992253e846b8afc7ec
sudikshakamath <83342160+sudikshakamath@users.noreply.github.com>
2024-09-06 11:46:39 +0530

    Update Calculater.java

--- Calculater.java
+++ Calculater.java
@@ -9,7 +9,7 @@ public class Calculator {
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println("10 + 6 = " + calc.add(10, 5));
+        System.out.println("11 + 6 = " + calc.add(11, 6));
         System.out.println("10 - 5 = " + calc.subtract(10, 5));
     }
 }
